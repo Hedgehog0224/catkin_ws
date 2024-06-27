af2198ab76259ce82e148e62ac691378f1656ac3
Lebedeva Ekaterina <161046982+Hedgehog0224@users.noreply.github.com>
2024-06-27 17:53:22 +0300

    Update chooseMode.py

--- src/robot_pkg/scripts/chooseMode.py
+++ src/robot_pkg/scripts/chooseMode.py
@@ -24,7 +24,9 @@ class chooseModeClass():
     elif data.axes[5] < 0:
         chooseModeClass.msg.mode = 2
     else: chooseModeClass.msg.mode = 0
-
+    chooseModeClass.msg.x = data.axes[4]
+    chooseModeClass.msg.y = data.axes[3]
+    chooseModeClass.msg.angle = data.axes[0]
     chooseModeClass.pubMode.publish(chooseModeClass.msg)
 
 robot = chooseModeClass()
