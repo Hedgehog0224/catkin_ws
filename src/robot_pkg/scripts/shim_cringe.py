f3304598607f6767b0acf70a717100aa548e1fd1
Lebedeva Ekaterina <161046982+Hedgehog0224@users.noreply.github.com>
2024-06-28 12:23:13 +0300

    Update shim_cringe.py

--- src/robot_pkg/scripts/shim_cringe.py
+++ src/robot_pkg/scripts/shim_cringe.py
@@ -22,6 +22,7 @@ if GPIO.getmode() == None:
 from MatMotors import Route, Motor
 
 class robotcl():
+    I2Cpins = [0,1,2,5,3,4,6,8,7,11,9,10]
     mode = 0
     varStopAll = 0
     JoySpeed = [0.0, 0.0]
@@ -44,9 +45,9 @@ class robotcl():
     @staticmethod
     def callback_scan(data):
         if robotcl.varStopAll:
-            robotcl.abcd.move(robotcl.abcd.set_speed(1, 1), [0, 0, 0, 0])
+            robotcl.abcd.move(robotcl.abcd.set_speed(1, 1), [0, 0, 0, 0], robotcl.I2Cpins)
         elif robotcl.mode in [0,3]:
-            robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0])
+            robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0], robotcl.I2Cpins)
             rospy.loginfo('The mode in which the robot does not drive is selected: %s', robotcl.mode)
         elif robotcl.mode == 2:
             size = int(len(data.ranges))
@@ -56,7 +57,7 @@ class robotcl():
             if temp > 0.6:
                 # print('stop')               
                 rospy.loginfo('No obstacles detected, the robot is in rest mode: %s', round(temp, 2))
-                robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0])
+                robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0], robotcl.I2Cpins)
             else:
                 # print('move')
                 res = [i for i, j in enumerate(data.ranges) if j == temp]
@@ -72,16 +73,16 @@ class robotcl():
                 # pub_xy(x, y)
                 
                 ArrForMove = robotcl.abcd.set_speed(x, y)
-                robotcl.abcd.move(ArrForMove, robotcl.PredArrForMove)
+                robotcl.abcd.move(ArrForMove, robotcl.PredArrForMove, robotcl.I2Cpins)
                 #abcd.move([1,1,0,0], [0,0,0,0])
         
                 if time.time() - start_time > 360:
-                    robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0])
+                    robotcl.abcd.move([0, 0, 0, 0], [0, 0, 0, 0], robotcl.I2Cpins)
                     sys.exit()
                 robotcl.PredArrForMove = ArrForMove
         else:            
             JoyArr = robotcl.abcd.set_speed(robotcl.JoySpeed[0], robotcl.JoySpeed[1], turnOsSys=45, ModeOfAngles = 1, FuncOfAngel = [0, robotcl.JoyAngle])
-            robotcl.abcd.move(JoyArr, [0,0,0,0])
+            robotcl.abcd.move(JoyArr, [0,0,0,0], robotcl.I2Cpins)
     
     @staticmethod
     def callback_ultra_zd(data):
