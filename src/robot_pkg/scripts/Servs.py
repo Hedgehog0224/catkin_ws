ea487b1b32990f3b8a50db7f7fbcf4bb026b5589
Lebedeva Ekaterina <161046982+Hedgehog0224@users.noreply.github.com>
2024-09-06 13:42:04 +0300

    Update Servs.py

--- src/robot_pkg/scripts/Servs.py
+++ src/robot_pkg/scripts/Servs.py
@@ -17,18 +17,18 @@ class startServs():
     self.pca = PCA9685(self.i2c)
     self.pca.frequency = 100
     startServs.servo10 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
-    startServs.servo11 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
-    startServs.servo12 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
-    startServs.servo13 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
-    startServs.servo14 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
-    startServs.servo15 = servo.Servo(self.pca.channels[10], actuation_range = 180, min_pulse = 750, max_pulse=2250)
+    startServs.servo11 = servo.Servo(self.pca.channels[11], actuation_range = 180, min_pulse = 750, max_pulse=2250)
+    startServs.servo12 = servo.Servo(self.pca.channels[12], actuation_range = 180, min_pulse = 750, max_pulse=2250)
+    startServs.servo13 = servo.Servo(self.pca.channels[13], actuation_range = 180, min_pulse = 750, max_pulse=2250)
+    startServs.servo14 = servo.Servo(self.pca.channels[14], actuation_range = 180, min_pulse = 750, max_pulse=2250)
+    startServs.servo15 = servo.Servo(self.pca.channels[15], actuation_range = 180, min_pulse = 750, max_pulse=2250)
     
     rospy.init_node('Servos')
 
     rospy.Subscriber("joy", Joy, self.callback_joy)
   @staticmethod
   def callback_joy(data):
-    # print(data)
+    print(data)
     #huj
     if data.buttons[1] == 1:
       startServs.angls[0] = 1
@@ -43,17 +43,17 @@ class startServs():
     if data.buttons[6] == 1:
       startServs.angls[0] = 6
 
-    if startServs.angls[startServs.angls[0]] > 20 and startServs.angls[startServs.angls[0]] < 160:
-      if data.axes[0] < 0:
-        startServs.angls[startServs.angls[0]] = startServs.angls[startServs.angls[0]] - 10
-      if data.axes[0] > 0:
-        startServs.angls[startServs.angls[0]] = startServs.angls[startServs.angls[0]] + 10
-    startServs.servo10 = startServs.angls[1]
-    startServs.servo11 = startServs.angls[2]
-    startServs.servo12 = startServs.angls[3]
-    startServs.servo13 = startServs.angls[4]
-    startServs.servo14 = startServs.angls[5]
-    startServs.servo15 = startServs.angls[6]
+    if data.axes[0] < 0 and startServs.angls[startServs.angls[0]] > 20:
+      startServs.angls[startServs.angls[0]] = startServs.angls[startServs.angls[0]] - 10
+    if data.axes[0] > 0 and startServs.angls[startServs.angls[0]] < 160:
+      startServs.angls[startServs.angls[0]] = startServs.angls[startServs.angls[0]] + 10
+    
+    startServs.servo10.angle = startServs.angls[1]
+    startServs.servo11.angle = startServs.angls[2]
+    startServs.servo12.angle = startServs.angls[3]
+    startServs.servo13.angle = startServs.angls[4]
+    startServs.servo14.angle = startServs.angls[5]
+    startServs.servo15.angle = startServs.angls[6]
   
   def publis2topic(self):
     self.srvData.servo0 = self.servo10.angle
