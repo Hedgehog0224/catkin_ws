9ca2deef2512dd2942a1984762685bea3e6324f4
Vladislav_Lozhkin <80119639+Sabotazhnyk@users.noreply.github.com>
2024-09-10 19:24:09 +0300

    Update test-joy.py

--- src/robot_pkg/scripts/test-joy.py
+++ src/robot_pkg/scripts/test-joy.py
@@ -1,10 +1,15 @@
 import rospy
 from sensor_msgs.msg import Joy
-def callback_joy(data):
+
+def callback_joy(data) -> None:
+  ```
+  Вывод данных с джойстика в терминал для тестирования кнопок (вывода данных)
+  ```
   print(data.axes)
   print(data.buttons)
+
+
 rospy.init_node('Get_Data_From_Joy')
 rospy.Subscriber("scan", Joy, callback_joy)
-
 rospy.sleep(0.05)
 rospy.spin()
